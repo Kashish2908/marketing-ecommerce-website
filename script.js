61995b6c7365d9e5b0bb60951fc5a648bb555c19
Kashish2908 <kneekhra29@gmail.com>
2023-10-05 14:43:01 +0530

    added side menu

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ function scrollTopfunc() {
 }
 
 scrollTopfunc()
+
+function openSideMenu(){
+    document.querySelector(".responsive-navbar .menu").addEventListener("click", function () {
+        document.querySelector(".side-menu").style.transform = "translateX(0)";
+        document.querySelector(".side-menu").style.opacity = 1;
+
+    })
+}
+
+openSideMenu()
+
+function closeSideMenu(){
+    document.querySelector(".side-menu .close").addEventListener("click", function () {
+        document.querySelector(".side-menu").style.transform = "translateX(100%)";
+        document.querySelector(".side-menu").style.opacity = 0;
+
+    })
+}
+
+closeSideMenu()
+
